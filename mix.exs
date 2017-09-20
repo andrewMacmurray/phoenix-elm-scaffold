2d7cc2b3834d7cd154dc672e65ee099eb8d66a45
Andrew MacMurray <a.macmurray@icloud.com>
2017-09-20 08:55:32 +0100

    bump version to 0.2.2

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ElmScaffold.Mixfile do
   def project do
     [app: :phoenix_elm_scaffold,
      name: "Phoenix Elm Scaffold",
-     version: "0.2.1",
+     version: "0.2.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
